485708edb5c8f8758d0677b22e4b6498df3aad80
DP94 <d.pell1994@gmail.com>
2022-07-09 18:08:50 +0100

    Use hash routing as S3 bucket doesn't like /

--- book-club-ui/src/router/index.ts
+++ book-club-ui/src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 import MainPageView from "../views/MainPageView.vue";
 import BookView from "../views/BookView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
